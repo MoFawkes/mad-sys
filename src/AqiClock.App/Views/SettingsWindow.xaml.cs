519b5cf0bc59ee2c8c007eac0a88671f09ecfe32
MK <mkkhilji94@gmail.com>
2026-07-18 00:02:06 +0100

    feat: redesign Settings with Fluent cards

--- src/AqiClock.App/Views/SettingsWindow.xaml.cs
+++ src/AqiClock.App/Views/SettingsWindow.xaml.cs
@@ -1,4 +1,6 @@
-using System.Windows;
 using AqiClock.App.ViewModels;
+using Wpf.Ui.Controls;
+
 namespace AqiClock.App.Views;
-public partial class SettingsWindow : Window { public SettingsWindow(SettingsViewModel viewModel) { InitializeComponent(); DataContext = viewModel; Closed += (_, _) => viewModel.Dispose(); } }
+
+public partial class SettingsWindow : FluentWindow { public SettingsWindow(SettingsViewModel viewModel) { InitializeComponent(); DataContext = viewModel; Closed += (_, _) => viewModel.Dispose(); } }
